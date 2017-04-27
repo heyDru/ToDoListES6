9b07060ad0061b0cb3ab1519e36d4029b182dba9
andrew <hey.dzyu@gmail.com>
2017-04-27 15:48:31 +0300

    Try to add delete btn

--- apps/js/services/Markup.js
+++ apps/js/services/Markup.js
@@ -8,13 +8,35 @@ export class Markup {
         document.write(html);
     }
 
-      GenerateItemMarkup(toDoList) {
+    GenerateItemMarkup(toDoList) {
         let listSection = document.getElementById('listSection');
         let listElements = document.getElementById('toDoList-template').innerHTML;
-        let html = _.template(listElements);
 
-        listSection.innerHTML=html({toDoList});
+        let transferToDoList = [];
+
+        _.each(toDoList, function (toDo) {
+            let deleteButton = document.createElement('button');
+            let deleteIcon = document.createElement('span');
+            deleteIcon.className = "glyphicon glyphicon-remove";
+
+            deleteButton.appendChild(deleteIcon);
+            deleteButton.className = "btn btn-danger btn-md";
+
+     
+            transferToDoItem.toDo = toDo;
+            transferToDoItem.btn = deleteButton;
+
+            transferToDoList.push(transferToDoItem);
+        });
+        let html = _.template(listElements);
+        console.log(transferToDoList);
+        listSection.innerHTML = html({ transferToDoList });
     }
 
 }
 
+
+        // let button = document.createElement('button');
+        // button.addEventListener('click', function (event) {
+
+        // });
